4f55d219ecd33b54e0ed2e55353819505c3d64b0
Park, seun <seunpark04@gmail.com>
2024-07-07 23:36:13 +0900

    Feat:프로필 구현

--- src/main/java/com/hoodie/otti/exception/profile/GlobalExceptionHandler.java
+++ src/main/java/com/hoodie/otti/exception/profile/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.hoodie.otti.exception.profile;
 
-import com.hoodie.otti.exception.profile.UserProfileNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
