319c6ca4bd398e119c610f50954313662f62630c
Park, seun <seunpark04@gmail.com>
2024-07-30 15:33:42 +0900

    Remove: ApplicationTest_contextLoads

--- src/test/java/com/hoodie/otti/HoodieOttiApplicationTests.java
+++ src/test/java/com/hoodie/otti/HoodieOttiApplicationTests.java
@@ -1,32 +1,14 @@
 package com.hoodie.otti;
 
-import com.hoodie.otti.repository.notification.NotificationRepository;
-import com.hoodie.otti.service.notification.NotificationService;
-import com.hoodie.otti.service.user.KakaoOAuthService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
 class HoodieOttiApplicationTests {
 
-	@Autowired
-	private NotificationService notificationService;
-
-	@Autowired
-	private KakaoOAuthService kakaoOAuthService;
-
-	@Autowired
-	private NotificationRepository notificationRepository;
-
 	@Test
 	void contextLoads() {
-		assertThat(notificationService).isNotNull();
-		assertThat(kakaoOAuthService).isNotNull();
-		assertNotNull(notificationRepository);
+
 	}
 }
