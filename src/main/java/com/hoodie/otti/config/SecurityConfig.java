a4349b424d0db0a995030c63f559c8b795a1cd6e
Park, Sojeong <sojeong2101@gmail.com>
2024-08-08 23:25:25 +0900

    Modify: cors 에러 해결을 위한 포트 변경

--- src/main/java/com/hoodie/otti/config/SecurityConfig.java
+++ src/main/java/com/hoodie/otti/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:8080"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
