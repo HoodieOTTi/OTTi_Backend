0a4549f2e4d06f14ac11e6a863fd7428cc612d64
Park, Sojeong <sojeong2101@gmail.com>
2024-08-11 00:05:47 +0900

    Modify: 카카오 로그인 dto 수정

--- src/main/java/com/hoodie/otti/dto/login/KakaoTokenDto.java
+++ src/main/java/com/hoodie/otti/dto/login/KakaoTokenDto.java
@@ -12,20 +12,20 @@ import lombok.Getter;
 @AllArgsConstructor
 public class KakaoTokenDto {
 
-    @SerializedName("access_token")
+    @JsonProperty("access_token")
     private String accessToken;
 
-//    @JsonProperty("token_type")
-//    private String tokenType;
+    @JsonProperty("token_type")
+    private String tokenType;
 
-    @SerializedName("refresh_token")
+    @JsonProperty("refresh_token")
     private String refreshToken;
 
-//    @JsonProperty("expires_in")
-//    private int expiresIn;
-//
-//    @JsonProperty("scope")
-//    private String scope;
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
 
     @Data
     @Builder
