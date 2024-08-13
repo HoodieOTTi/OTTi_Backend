c1014b24c029f69336b70ac986c9a69b1d5784ba
Park, Sojeong <sojeong2101@gmail.com>
2024-08-13 23:46:48 +0900

    Modify: cors 에러 해결

--- src/main/java/com/hoodie/otti/config/SecurityConfig.java
+++ src/main/java/com/hoodie/otti/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://otti.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
