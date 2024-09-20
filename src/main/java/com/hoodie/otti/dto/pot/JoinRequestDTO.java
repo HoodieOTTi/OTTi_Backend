82dfd09f2d95fa72b26727a8f806e60eb9c8446b
Park, seun <seunpark04@gmail.com>
2024-09-21 01:11:30 +0900

    Modify: 조회시 ottDTO가 같이 들어도록 수정

--- src/main/java/com/hoodie/otti/dto/pot/JoinRequestDTO.java
+++ src/main/java/com/hoodie/otti/dto/pot/JoinRequestDTO.java
@@ -1,5 +1,6 @@
 package com.hoodie.otti.dto.pot;
 
+import com.hoodie.otti.dto.ott.OttResponseDto;
 import com.hoodie.otti.model.pot.JoinRequest;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,21 +11,26 @@ import lombok.Setter;
 public class JoinRequestDTO {
     private Long id;
     private String potName;
+    private OttResponseDto ott;
     private RequesterDTO requester;
     private Boolean approved;
 
     @Builder
-    public JoinRequestDTO(Long id, String potName, RequesterDTO requester, Boolean approved) {
+    public JoinRequestDTO(Long id, String potName, OttResponseDto ott, RequesterDTO requester, Boolean approved) {
         this.id = id;
         this.potName = potName;
+        this.ott = ott;
         this.requester = requester;
         this.approved = approved;
     }
 
     public static JoinRequestDTO fromEntity(JoinRequest joinRequest) {
+        OttResponseDto ottResponseDto = OttResponseDto.fromEntity(joinRequest.getPot().getOttId());
+
         return JoinRequestDTO.builder()
                 .id(joinRequest.getId())
                 .potName(joinRequest.getPot().getName())
+                .ott(ottResponseDto)
                 .requester(new RequesterDTO(
                         joinRequest.getRequester().getUsername(),
                         joinRequest.getRequester().getProfilePhotoUrl(),
